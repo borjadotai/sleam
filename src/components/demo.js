b1687047889684d22d1b8bbac4f5c71a9248e608
borjadotai <hi@borja.ai>
2020-06-01 09:05:39 +0100

    Fix warning

--- src/components/demo.js
+++ src/components/demo.js
@@ -17,7 +17,7 @@ const Demo = () => {
           width="100%"
           height="100%"
           src="https://www.youtube.com/embed/YIWccgxz5KE"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
         ></iframe>
